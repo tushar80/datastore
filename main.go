df217872525cd4073c5498afacc06174233b0141
Tushar Patil <patiltushar80@yahoo.com>
2024-08-10 23:45:47 +0530

    Setup gin server and configure database

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	"github.com/tushar80/datastore/config"
 )
 
 func main() {
@@ -11,4 +14,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	config.InitDB()
+	config.InitRedis()
+
+	r := gin.Default()
+
+	r.Run(":8080")
 }
